3041aef13b88afbd21842731be728f7c18ef56c9
Amadou <amadou_dieye@hotmail.fr>
2020-10-19 11:14:44 +0200

    adding e02.js

---
+++ e02.js
@@ -0,0 +1,15 @@
+function insertion(tab=[]) {
+    let tabTrier=[]
+    for (let i = 0; i < tab.length; i++) {
+        tmp = tab[i]
+        j = i - 1
+        while (j >= 0 && tab[j] > tmp) {
+          tab[j+1] = tab[j]
+          j--
+        }
+        tab[j+1] = tmp
+    }
+      return tab      
+}
+
+console.log(insertion([5,1,4,2,8]));//renvoie [1,2,4,5,8]
